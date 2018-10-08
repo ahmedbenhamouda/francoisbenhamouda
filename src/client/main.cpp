cdcef95fcba73c4a99310829438eeda936164c70
Benjamin Francois <benjamin.francois@ensea.fr>
2018-10-08 16:03:32 +0200

    corrigé le diagramme UML.

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -17,8 +17,8 @@ using namespace state;
 
 int main(int argc,char* argv[]) 
 {
-    Exemple exemple;
-    exemple.setX(53);
+    //Exemple exemple;
+    //exemple.setX(53);
 
     if (argc < 2) {
         cout << "Missing the \"hello\" argument." << endl;
