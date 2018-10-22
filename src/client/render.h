f926f21b6f2895cff15651d509d6ea589ab38577
Ahmed Ben Hamouda <ahmed.ben-hamouda@ensea.fr>
2018-10-22 15:05:24 +0200

    Tile

--- src/client/render.h
+++ src/client/render.h
@@ -3,4 +3,9 @@
 #define __RENDER__H
 
 #include "render/Tilemap.h"
+#include "render/Tile.h"
+#include "render/TileSet.h"
+#include "render/TerrainTileSet.h"
+#include "render/UniteTileSet.h"
+#include "render/BatimentTileSet.h"
 #endif
