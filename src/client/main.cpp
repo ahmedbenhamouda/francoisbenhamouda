53f69b64ca0bb73619231dff25c6faeab33838d3
Ahmed Ben Hamouda <ahmed.ben-hamouda@ensea.fr>
2018-11-14 10:01:00 +0100

    map

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -5,7 +5,7 @@
 #include <thread>
 #include <mutex>
 #include <chrono>
-
+#include "state/carte.h"
 // Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
 
@@ -223,29 +223,8 @@ void engineTest() {
     unique_ptr<Batiment> batiment2 (new Usine(Position(5, 2), 2));
 
     // création d'un objet TerrainTab
-    vector<vector<TerrainTypeId>> defaultVector { 
-	{plaine,plaine,plaine,plaine,plaine,foret,foret,plaine,plaine,plaine,plaine,plaine,plaine,foret,foret,plaine,plaine,plaine,plaine,plaine},
-	{route,route,route,route,plaine,foret,foret,plaine,plaine,plaine,plaine,plaine,plaine,foret,foret,plaine,route,route,route,route},
-	{montagne,plaine,plaine,route,plaine,foret,foret,foret,foret,plaine,plaine,foret,foret,foret,foret,plaine,route,plaine,plaine,montagne},
-	{montagne,montagne,plaine,route,plaine,plaine,plaine,plaine,foret,foret,foret,foret,plaine,plaine,plaine,plaine,route,plaine,montagne,montagne},
-	{plaine,foret,plaine,route,route,route,plaine,plaine,montagne,foret,foret,montagne,plaine,plaine,route,route,route,plaine,foret,plaine},
-	{plaine,foret,foret,route,plaine,route,route,route,route,route,route,route,route,route,route,plaine,route,foret,foret,plaine},
-	{plaine,plaine,route,route,montagne,montagne,plaine,plaine,foret,foret,foret,foret,plaine,montagne,montagne,plaine,route,route,plaine,plaine},
-	{plaine,plaine,route,plaine,plaine,plaine,plaine,plaine,foret,montagne,montagne,foret,plaine,plaine,plaine,plaine,plaine,route,plaine,plaine},
-	{plaine,plaine,route,plaine,plaine,plaine,plaine,plaine,foret,montagne,montagne,foret,plaine,plaine,plaine,plaine,plaine,route,plaine,plaine},
-	{plaine,plaine,route,plaine,plaine,plaine,plaine,plaine,montagne,montagne,montagne,montagne,plaine,plaine,plaine,plaine,plaine,route,plaine,plaine},
-	{plaine,plaine,route,plaine,plaine,plaine,plaine,plaine,montagne,montagne,montagne,montagne,plaine,plaine,plaine,plaine,plaine,route,plaine,plaine},
-	{plaine,plaine,route,plaine,plaine,plaine,plaine,plaine,foret,montagne,montagne,foret,plaine,plaine,plaine,plaine,plaine,route,plaine,plaine},
-	{plaine,plaine,route,plaine,plaine,plaine,plaine,plaine,foret,montagne,montagne,foret,plaine,plaine,plaine,plaine,plaine,route,plaine,plaine},
-	{plaine,plaine,route,route,montagne,montagne,plaine,plaine,foret,foret,foret,foret,plaine,montagne,montagne,plaine,route,route,plaine,plaine},
-	{plaine,foret,foret,route,plaine,route,route,route,route,route,route,route,route,route,route,plaine,route,foret,foret,plaine},
-	{plaine,foret,plaine,route,route,route,plaine,plaine,montagne,foret,foret,montagne,plaine,plaine,route,route,route,plaine,foret,plaine},
-	{montagne,montagne,plaine,route,plaine,plaine,plaine,plaine,foret,foret,foret,foret,plaine,plaine,plaine,plaine,route,plaine,montagne,montagne},
-	{montagne,plaine,plaine,route,plaine,foret,foret,foret,foret,plaine,plaine,foret,foret,foret,foret,plaine,route,plaine,plaine,montagne},
-	{route,route,route,route,plaine,foret,foret,plaine,plaine,plaine,plaine,plaine,plaine,foret,foret,plaine,route,route,route,route},
-	{plaine,plaine,plaine,plaine,plaine,foret,foret,plaine,plaine,plaine,plaine,plaine,plaine,foret,foret,plaine,plaine,plaine,plaine,plaine}
-};
-    unique_ptr<TerrainTab> terrainTab (new TerrainTab(defaultVector));
+ 
+    unique_ptr<TerrainTab> terrainTab (new TerrainTab(map1()));
     
     // création d'un objet Terrain
     vector<Batiment*> batiments;
---
+++ src/shared/state/carte.h
@@ -0,0 +1,5 @@
+#include <vector>
+#include "TerrainTypeId.h"
+
+std::vector<std::vector<state::TerrainTypeId>> map1();
+
